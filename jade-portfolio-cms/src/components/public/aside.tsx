63e8dde458fc3e5510727d144d9a520940751a61
DESKTOP-QC78LBU\4M-FE <gasina85@gmail.com>
2026-03-20 16:49:23 +0900

    fix(public): aside 수정

--- jade-portfolio-cms/src/components/public/aside.tsx
+++ jade-portfolio-cms/src/components/public/aside.tsx
@@ -8,51 +8,84 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 
-export default function Aside() {
+type AsideProps = {
+  activeSection: string;
+  onNavClick: (section: string) => void;
+  isAsideOpen: boolean;
+  onToggle: () => void;
+};
+
+export default function Aside({
+  activeSection,
+  onNavClick,
+  isAsideOpen,
+  onToggle,
+}: AsideProps) {
   return (
-    <aside className="aside">
+    <aside className={isAsideOpen ? "aside open" : "aside"}>
       <div className="logo">
         <Link href="/">JADE</Link>
       </div>
 
-      <div className="nav-toggler">
+      <div
+        className={isAsideOpen ? "nav-toggler open" : "nav-toggler"}
+        onClick={onToggle}
+      >
         <span></span>
       </div>
 
       <ul className="nav">
         <li>
-          <a href="#home" className="active">
-            <House className="fa" size={15} /> Home
+          <a
+            href="#home"
+            className={activeSection === "home" ? "active" : ""}
+            onClick={() => onNavClick("home")}
+          >
+          <House className="fa" size={15} /> Home
           </a>
         </li>
         <li>
-          <a href="#about">
+          <a
+            href="#about"
+            className={activeSection === "about" ? "active" : ""}
+            onClick={() => onNavClick("about")}
+          >
             <User className="fa" size={15} /> About
           </a>
         </li>
         <li>
-          <a href="#services">
+          <a href="#services"
+            className={activeSection === "services" ? "active" : ""}
+          >
             <List className="fa" size={15} /> Service
           </a>
         </li>
         <li>
-          <a href="#portfolio">
+          <a
+            href="#portfolio"
+            className={activeSection === "portfolio" ? "active" : ""}
+            onClick={() => onNavClick("portfolio")}
+          >
             <Briefcase className="fa" size={15} /> Portfolio
           </a>
         </li>
         <li>
-          <a href="#project">
+          <a href="#project"
+            className={activeSection === "project" ? "active" : ""}
+          >
             <FolderKanban className="fa" size={15} /> Project
           </a>
         </li>
         <li>
-          <a href="#contact">
+          <a
+            href="#contact"
+            className={activeSection === "contact" ? "active" : ""}
+            onClick={() => onNavClick("contact")}
+          >
             <MessageCircle className="fa" size={15} /> Contact
           </a>
         </li>
       </ul>
-
-      <div className="copyright-text">© 2026 Jade Portfolio</div>
     </aside>
   );
 }
